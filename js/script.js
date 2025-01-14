73189ca706dc0f118fd1246a018ad2c5cf04f90b
Sophia Chirskaya <sophia.chirskaya@gmail.com>
2025-01-14 18:51:30 +0100

    [set] countdown

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 // assegno le variabili di input
 const inputUser = document.querySelectorAll(".form-control")
 console.log(inputUser);
+
+// creo contdowm
+let countdown = 5;
+// funzione per aggiornare il countdown
+const updateCountdown = () => {
+    console.log(countdown);
+    if (countdown === 0) {
+        console.log("Tempo scaduto!");
+        clearInterval(intervalId);
+    } else {
+        countdown--;
+    }
+}
+
+const intervalId = setInterval(updateCountdown, 1000);
+
+// genero i numeri randomici
